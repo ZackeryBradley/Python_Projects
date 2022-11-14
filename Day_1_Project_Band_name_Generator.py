9ffaa9b2b21823ab948d7f458016662f3c239ff5
Zackery Bradley <91440520+ZackeryBradley@users.noreply.github.com>
2022-11-14 16:35:53 -0500

    Add files via upload

---
+++ Day_1_Project_Band_name_Generator.py
@@ -0,0 +1,6 @@
+
+
+print("Welcome to the band name generator")
+city_name = input( "What's the name of the city you grew up in:")
+pet_name = input("What's a good name for a pet?")
+print("Your band name is the:" + " " + city_name +  " " + pet_name)
