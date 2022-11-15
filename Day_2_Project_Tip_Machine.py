c270edb53b0bfbc2d8f37acc261b499f4aad9aa3
Zackery Bradley <91440520+ZackeryBradley@users.noreply.github.com>
2022-11-15 15:11:17 -0500

    Add files via upload

---
+++ Day_2_Project_Tip_Machine.py
@@ -0,0 +1,15 @@
+
+
+print("Welcome to the tip calculator.")
+total_bill = input("What was the total bill?")
+tip_percentage = input("What percentage tip would you like to give? ")
+number_of_people = input("How many people are splitting the bill?")
+
+total_bill = float(total_bill)
+tip_percentage = float(tip_percentage) / 100
+number_of_people = int(number_of_people) 
+
+final_bill = (total_bill * tip_percentage) + (total_bill) / number_of_people
+
+
+print(f'Each person should pay : ${final_bill}')
