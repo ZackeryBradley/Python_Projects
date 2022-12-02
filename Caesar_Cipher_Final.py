1633905358982d3e309455573baff2b7fc5ae25e
Zackery Bradley, MSBA <91440520+ZackeryBradley@users.noreply.github.com>
2022-12-02 13:41:09 -0500

    Add files via upload

---
+++ Caesar_Cipher_Final.py
@@ -0,0 +1,40 @@
+
+####################
+#Caesar_Cipher_Final
+####################
+from Caesar_Cipher_Logo import logo
+print(logo)
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
+'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
+'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
+'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+should_continue = True
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    shift = shift % 26
+    caesar(start_text = text, shift_amount = shift, cipher_direction = direction)
+
+    restart = input("Type 'Yes' if you want to go again. Otherwise type 'no'.\n")
+    if restart == "no":
+        should_continue = False
+        print("You have ended the cipher. Goodbye.")
+
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    if cipher_direction == "decode":
+            #5 * -1 = -5
+            shift_amount *= -1
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
+    print(f"Here's the {cipher_direction}d result: {end_text}")
+
